37603e15349b6c5699801c0a4cd095e56db68af8
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-19 22:40:11 -0300

    Implementing a Service Layer class

---
+++ src/main/java/com/lassulfi/app/photoapp/api/users/service/UsersService.java
@@ -0,0 +1,6 @@
+package com.lassulfi.app.photoapp.api.users.service;
+
+public interface UsersService {
+
+	UserDto createUser(UserDto userDetails);
+}
---
+++ src/main/java/com/lassulfi/app/photoapp/api/users/service/UsersServiceImpl.java
@@ -0,0 +1,11 @@
+package com.lassulfi.app.photoapp.api.users.service;
+
+public class UsersServiceImpl implements UsersService {
+
+	@Override
+	public UserDto createUser(UserDto userDetails) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
