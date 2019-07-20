1b3781c96fb8b1b8910ec2edd0d18a18c78348e8
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-19 22:34:36 -0300

    Validate HTTP Request body

--- src/main/java/com/lassulfi/app/photoapp/api/users/ui/model/CreateUserRequestModel.java
+++ src/main/java/com/lassulfi/app/photoapp/api/users/ui/model/CreateUserRequestModel.java
@@ -1,10 +1,25 @@
 package com.lassulfi.app.photoapp.api.users.ui.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class CreateUserRequestModel {
 
+	@NotNull(message="First name cannot be null")
+	@Size(min=2, message="First name must not be less than {min} characters")
 	private String firstName;
+	
+	@NotNull(message="Last name cannot be null")
+	@Size(min=2, message="Last name must not be less than {min} characters")
 	private String lastName;
+	
+	@NotNull(message="Password cannot be  null")
+	@Size(min=8, max=16, message="Password must be equal or greater than {min} characters and less than {max} characters")
 	private String password;
+	
+	@NotNull(message="Email cannot be null")
+	@Email
 	private String email;
 	
 	public String getFirstName() {
