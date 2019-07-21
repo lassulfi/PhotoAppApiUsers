dc72de4211ddd7d04ca125b503d8528a65d0982d
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-21 10:58:58 -0300

    Generate Unique Public User Id

--- src/main/java/com/lassulfi/app/photoapp/api/users/service/UsersServiceImpl.java
+++ src/main/java/com/lassulfi/app/photoapp/api/users/service/UsersServiceImpl.java
@@ -1,12 +1,15 @@
 package com.lassulfi.app.photoapp.api.users.service;
 
+import java.util.UUID;
+
 import com.lassulfi.app.photoapp.api.users.shared.UserDto;
 
 public class UsersServiceImpl implements UsersService {
 
 	@Override
 	public UserDto createUser(UserDto userDetails) {
-		// TODO Auto-generated method stub
+		userDetails.setUserId(UUID.randomUUID().toString());
+
 		return null;
 	}
 
