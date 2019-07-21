66a85d6a688286f702b676626d8cb1fc209c4ec5
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-21 14:55:35 -0300

    Encrypt user password

--- src/main/java/com/lassulfi/app/photoapp/api/users/service/UsersServiceImpl.java
+++ src/main/java/com/lassulfi/app/photoapp/api/users/service/UsersServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.lassulfi.app.photoapp.api.users.data.UserEntity;
@@ -16,20 +17,23 @@ public class UsersServiceImpl implements UsersService {
 
 	private UserRepository userRespository;
 	
+	private BCryptPasswordEncoder passwordEncoder;
+	
 	@Autowired
-	public UsersServiceImpl(UserRepository userRepository) {
+	public UsersServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
 		this.userRespository = userRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 	
 	@Override
 	public UserDto createUser(UserDto userDetails) {
 		userDetails.setUserId(UUID.randomUUID().toString());
+		userDetails.setEncryptedPassword(passwordEncoder.encode(userDetails.getPassword()));
 		
 		ModelMapper mapper = new ModelMapper();
 		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		
 		UserEntity userEntity = mapper.map(userDetails, UserEntity.class);
-		userEntity.setEncryptedPassword("test");
 
 		userRespository.save(userEntity);
 		
