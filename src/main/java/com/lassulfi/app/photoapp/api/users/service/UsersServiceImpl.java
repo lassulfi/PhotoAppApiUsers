8d8568bb23964b963cc1b4dacf35dedc1b4d55a9
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-07-31 07:16:46 -0300

    Make Users Microservice call Albums Microservice

--- src/main/java/com/lassulfi/app/photoapp/api/users/service/UsersServiceImpl.java
+++ src/main/java/com/lassulfi/app/photoapp/api/users/service/UsersServiceImpl.java
@@ -1,32 +1,45 @@
 package com.lassulfi.app.photoapp.api.users.service;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.lassulfi.app.photoapp.api.users.data.UserEntity;
 import com.lassulfi.app.photoapp.api.users.data.UserRepository;
 import com.lassulfi.app.photoapp.api.users.shared.UserDto;
+import com.lassulfi.app.photoapp.api.users.ui.model.AlbumResponseModel;
 
 @Service
 public class UsersServiceImpl implements UsersService {
 
 	private UserRepository userRepository;
-	
 	private BCryptPasswordEncoder passwordEncoder;
+	private RestTemplate restTemplate;
+	private Environment environment;
 	
 	@Autowired
-	public UsersServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+	public UsersServiceImpl(UserRepository userRepository, 
+			BCryptPasswordEncoder passwordEncoder,
+			RestTemplate restTemplate,
+			Environment environment) {
 		this.userRepository = userRepository;
 		this.passwordEncoder = passwordEncoder;
+		this.restTemplate = restTemplate;
+		this.environment = environment;
 	}
 	
 	@Override
@@ -67,10 +80,20 @@ public class UsersServiceImpl implements UsersService {
 	@Override
 	public UserDto getUserById(String userId) {
 		UserEntity userEntity = userRepository.findByUserId(userId);
-		if(userEntity == null) throw new UsernameNotFoundException("User not found")
+		if(userEntity == null) throw new UsernameNotFoundException("User not found");
 		
-		UserDto userDto = new ModelMapper().map(userEntity, UserDto.class); 
+		UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
+		
+		String albumsUrl = String.format(environment.getProperty("albums.url"), userId);
 				
+		ResponseEntity<List<AlbumResponseModel>> albumsListResponse = restTemplate
+				.exchange(albumsUrl, 
+						HttpMethod.GET, 
+						null,
+						new ParameterizedTypeReference<List<AlbumResponseModel>>() {});
+		List<AlbumResponseModel> albumsList = albumsListResponse.getBody();
+		userDto.setAlbums(albumsList);
+		
 		return userDto;
 	}
 }
